209847708680d723a9a86830aa781b5cbab3614f
Félix Chambragne <felix.chambragne@gmail.com>
2023-04-26 17:51:47 +0200

    edit

---
+++ test_raspi/test_i2c_graph.py
@@ -0,0 +1,39 @@
+import smbus
+import time
+import csv
+import matplotlib.pyplot as plt
+
+bus = smbus.SMBus(1)
+
+def read_pcf8591():
+    bus.write_byte(0x48, 0x40)
+    return bus.read_byte(0x48)
+
+values = []
+timestamps = []
+
+# Set the duration of the data collection
+duration = 10 # seconds
+
+start_time = time.time()
+
+# Collect data for the specified duration
+while time.time() - start_time < duration:
+    value = read_pcf8591()
+    timestamp = time.time()
+    values.append(value)
+    timestamps.append(timestamp)
+    time.sleep(0.05)
+
+# Save the data to a CSV file
+with open('data.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['timestamp', 'value'])
+    for i in range(len(values)):
+        writer.writerow([timestamps[i], values[i]])
+
+# Plot the data
+plt.plot(timestamps, values)
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.savefig('plot.png')
