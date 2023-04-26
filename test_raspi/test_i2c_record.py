9fb4c928d94979e3a4479345f0c95b2321a78532
Félix Chambragne <felix.chambragne@gmail.com>
2023-04-26 17:33:53 +0200

    edit

--- test_raspi/test_i2c_record.py
+++ test_raspi/test_i2c_record.py
@@ -3,25 +3,41 @@ import pyaudio
 import wave
 
 print(pyaudio.get_portaudio_version())
+
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
+
 bus = smbus.SMBus(1)
 bus.write_byte(0x48, 0x40)
+
 p = pyaudio.PyAudio()
+
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
+
 frames = []
+
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
     analog_value = bus.read_byte(0x48)
     print(analog_value)
+
 stream.stop_stream()
 stream.close()
+p.terminate()
+
+# write data to a WAV file
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
