bd1c776d234ae5e32db25cc3aa6a80fb4dc08a18
Félix Chambragne <felix.chambragne@gmail.com>
2023-04-29 19:20:32 +0200

    e

--- test_raspi/test_beat.py
+++ test_raspi/test_beat.py
@@ -1,55 +1,47 @@
-import smbus
+import pyaudio
 import numpy as np
-from scipy import signal
+from scipy.fftpack import fft
+import smbus
 
-# set up I2C communication
-I2C_ADDRESS = 0x48  # replace with your ADC's I2C address
-bus = smbus.SMBus(1)  # replace with the correct I2C bus number
+# Variables de configuration
+CHUNK = 1024
+RATE = 44100
+THRESHOLD = 12000
+SILENCE_LIMIT = 10
+DETECT_LENGTH = 50
 
-# define the ADC channels
-CHANNEL_LEFT = 0  # replace with the correct channel number for the left channel
-CHANNEL_RIGHT = 1  # replace with the correct channel number for the right channel
+# Initialisation de l'interface I2C
+bus = smbus.SMBus(1)
 
-# set up the beat detection parameters
-FRAME_SIZE = 2048  # number of samples per frame
-HOP_SIZE = 1024  # number of samples between frames
-BPM_RANGE = (60, 180)  # range of possible BPM values
-ENERGY_THRESHOLD_RATIO = 1.5  # ratio of current energy to average energy threshold for beat detection
+def read_pcf8591():
+    bus.write_byte(0x48, 0x40)
+    return bus.read_byte(0x48)
 
-# calculate the frequency range for the bandpass filter
-SAMPLING_RATE = 44100  # replace with your sampling rate
-BANDPASS_FREQ_RANGE = [70, 150]  # replace with your desired frequency range
-BANDPASS_ORDER = 3  # order of the filter
-nyquist = 0.5 * SAMPLING_RATE
-b, a = signal.butter(BANDPASS_ORDER, np.array(BANDPASS_FREQ_RANGE) / nyquist, btype='band')
-#BANDPASS_COEFFS = librosa.filters.band_pass(SAMPLING_RATE, BANDPASS_FREQ_RANGE[0], BANDPASS_FREQ_RANGE[1], BANDPASS_ORDER)
+# Initialisation de PyAudio
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-# initialize the energy and beat variables
-energy = np.zeros((FRAME_SIZE // 2 + 1,))
-average_energy = 1e-9
-beat = False
-
-# read audio data from the module and detect beats
+# Boucle de détection de beat
+beat_detected = False
+silence_count = 0
 while True:
-    # read the left channel data
-    bus.write_byte(I2C_ADDRESS, 0x40 | (CHANNEL_LEFT << 4))
-    data_l = bus.read_word_data(I2C_ADDRESS, 0x00)
-    # read the right channel data
-    bus.write_byte(I2C_ADDRESS, 0x40 | (CHANNEL_RIGHT << 4))
-    data_r = bus.read_word_data(I2C_ADDRESS, 0x00)
-    # convert to float and normalize
-    data = np.array([data_l, data_r], dtype=np.float32)
-    data = (data / 1024.0) - 1.0
-    # apply the bandpass filter and calculate the energy
-    data_filtered = signal.filtfilt(b, a, data, axis=0)
-    data_envelope = np.abs(signal.hilbert(data_filtered, axis=0))
-    energy = 0.9 * energy + 0.1 * data_envelope ** 2
-    average_energy = 0.99 * average_energy + 0.01 * np.mean(energy, axis=0)
-    # detect beat if the energy exceeds the threshold
-    if np.mean(energy) > ENERGY_THRESHOLD_RATIO * average_energy:
-        beat = True
-        print("beat detected")
-    else:
-        beat = False
-    
-    # TODO: do something with the beat
+    # Lecture des échantillons audio
+    data = stream.read(CHUNK)
+    audio_data = np.frombuffer(data, dtype=np.int16)
+
+    # Calcul de la FFT
+    fft_data = fft(audio_data)
+    freqs = np.fft.fftfreq(len(fft_data))
+
+    # Obtention de la puissance des fréquences basses
+    low_freqs = np.where(freqs >= 0)[0][:len(freqs)//2]
+    power = np.abs(fft_data[low_freqs])**2
+
+    # Détection de beat
+    if np.max(power) > THRESHOLD and not beat_detected:
+        beat_detected = True
+        silence_count = 0
+    elif beat_detected:
+        silence_count += 1
+        if silence_count >= SILENCE_LIMIT:
+            beat_detected = False
