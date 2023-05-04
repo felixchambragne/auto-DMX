07b83b55ec95dd9c77621e4d854bec1126cc1c22
Félix Chambragne <felix.chambragne@gmail.com>
2023-05-04 16:55:54 +0200

    e

--- test_raspi/test_beat3.py
+++ test_raspi/test_beat3.py
@@ -38,16 +38,16 @@ while time.time() - start_time < duration:
     bass_indices = [idx for idx,val in enumerate(freqs) if val >= 20 and val <= 90]
 
     bass = np.max(psd[bass_indices])
-    bass_max = max(bass_max, bass)*0.9
+    bass_max = max(bass_max, bass)*0.8
 
-    if bass >= bass_max*.8 and not bass_beat:
+    if bass >= bass_max*0.8 and not bass_beat:
         bass_beat = True
         beat_count += 1
         print("bass", round(bass*100, 2), "bass_max", round(bass_max*100, 2), "Beat", beat_count, "             ", end='\r')
-    elif bass < bass_max*.5:
+    elif bass < bass_max*0.5:
         bass_beat = False
     
-    bass_max *= 0.99
+    bass_max *= 0.95
 
     # Ajuster le seuil en fonction de la valeur maximale de la PSD
     if np.max(psd) > 1:
