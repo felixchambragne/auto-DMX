d675d73405e5eddf0023ae6129cccef085087797
Félix Chambragne <felix.chambragne@gmail.com>
2023-05-08 13:54:17 +0200

    e

--- test_raspi/plot_data.py
+++ test_raspi/plot_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV file
-df = pd.read_csv('test_raspi/data2.csv')
+df = pd.read_csv('test_raspi/data1.csv')
 
 # Extract the data from the DataFrame
 timestamps = df['timestamp']
@@ -13,17 +13,17 @@ values = df['value']
 plt.stem(timestamps, values, markerfmt=' ')
 plt.xlabel('Time')
 plt.ylabel('Value')
-plt.savefig('plot2.png')
+plt.savefig('plot1.png')
 
 # Tracer le spectrogramme
-fig, ax = plt.subplots(figsize=(8, 4))
+"""fig, ax = plt.subplots(figsize=(8, 4))
 spec = ax.specgram(values, Fs=1000, NFFT=1024, noverlap=512, mode='magnitude', scale='dB')
 
 # Ajuster l'échelle logarithmique pour l'axe des fréquences
 ax.set_yscale('log')
 
 # Ajouter une étiquette pour l'axe des fréquences
-ax.set_ylabel('Frequency [Hz]')
+ax.set_ylabel('Frequency [Hz]')"""
 
 # Afficher le spectrogramme
 plt.show()
