1883809efb47594d8ec92f0054925d7e32ba6195
Félix Chambragne <felix.chambragne@gmail.com>
2023-05-21 20:06:11 +0200

    e

--- app/device.py
+++ app/device.py
@@ -40,6 +40,7 @@ class Device():
             else:
                 if fade_value < target_value:
                     fade_value = target_value
+            print(fade_value)
 
             self.set_data(self.address, self.channels["intensity"], fade_value)
             time.sleep(DMX_UPDATE_INTERVAL / 1000)
