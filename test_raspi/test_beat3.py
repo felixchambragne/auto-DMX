ad71987ea39bfe5895a08f4113aae41a216a1b45
Félix Chambragne <felix.chambragne@gmail.com>
2023-05-24 18:15:14 +0200

    e

--- test_raspi/test_beat3.py
+++ test_raspi/test_beat3.py
@@ -27,44 +27,28 @@ class BeatDetection():
     def detect_bass(self):
         bass_indices = [idx for idx,val in enumerate(self.freqs) if val >= 20 and val <= 90]
 
-        self.bass = np.max(self.psd[bass_indices])
-        self.bass_max = max(self.bass_max, self.bass)*0.8
-        if self.bass >= self.bass_max*0.8 and not self.bass_beat:
+        bass = np.max(self.psd[bass_indices])
+        self.bass_max = max(self.bass_max, bass)*0.8
+        if bass >= self.bass_max*0.8 and not self.bass_beat:
             self.bass_beat = True
-            print("bass", round(self.bass*100, 2), "bass_max", round(self.bass_max*100, 2), "Beat", "             ", end='\r')
-        elif self.bass < self.bass_max*0.5:
+            print("OOOOO bass", round(bass*100, 2), "bass_max", round(self.bass_max*100, 2), "Beat", "             ", end='\r')
+        elif bass < self.bass_max*0.5:
             self.bass_beat = False
         self.bass_max *= 0.95
-
-        print("\n", end='\r')
-
-        if self.bass_beat == True:
-            print("BEAT", end='\r')
-        else:
-            print("     ", end='\r')
 
     def detect_mid(self):
         mid_indices = [idx for idx,val in enumerate(self.freqs) if val >= 90 and val <= 300]
 
-        self.mid = np.max(self.psd[mid_indices])
-        self.mid_max = max(self.mid_max, self.mid)*0.8
-        if self.mid >= self.mid_max*0.8 and not self.mid_beat:
+        mid = np.max(self.psd[mid_indices])
+        self.mid_max = max(self.mid_max, mid)*0.8
+        if mid >= self.mid_max*0.8 and not self.mid_beat and not self.bass_beat:
             self.mid_beat = True
-            print("mid", round(self.mid*100, 2), "mid_max", round(self.mid_max*100, 2), "Beat", "             ", end='\r')
-        elif self.mid < self.mid_max*0.5:
+            print("----- mid", round(mid*100, 2), "mid_max", round(self.mid_max*100, 2), "Beat", "             ", end='\r')
+        elif mid < self.mid_max*0.5:
             self.mid_beat = False
         self.mid_max *= 0.95
 
-        print("\n", end='\r')
-
-        if self.mid_beat == True:
-            print("BEAT", end='\r')
-        else:
-            print("     ", end='\r')
-
     def run(self):
-        print("start")
-
         while True:
             for i in range(self.data.shape[0]):
                 value = self.read_pcf8591()
@@ -73,9 +57,11 @@ class BeatDetection():
             self.freqs, self.psd = signal.welch(self.data, self.framerate, nperseg=self.sample_size)
             peaks, _ = signal.find_peaks(self.psd, height=0.1*np.max(self.psd), distance=50)
 
+
             self.detect_bass()
-            if self.bass_beat == False:
-                self.detect_mid()
+            self.detect_mid()
+
+            print("\n", end='\r')
 
             time.sleep(0.001)
 
