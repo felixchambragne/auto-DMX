a42feb70557f938d68631ff700651caeaec427a9
Félix Chambragne <felix.chambragne@gmail.com>
2023-07-20 11:50:12 +0200

    a

---
+++ app/serial_thread.py
@@ -0,0 +1,23 @@
+import threading
+import serial
+
+class SerialThread(threading.Thread):
+    def __init__(self, app):
+        threading.Thread.__init__(self)
+        self.app = app
+
+        self.ser = serial.Serial('/dev/USB0', 9600)
+
+    def send_serial(self, data:str):
+        self.ser.write(data.encode())
+
+    def receive_serial(self):
+        return self.ser.readline().decode().strip()
+
+    def run(self):
+        self.send_serial("Hello from Raspberry Pi!")
+        while True:
+            received_data = self.receive_serial()
+            if received_data == "BUTTON_PRESSED":
+                print("Received data:", received_data)
+
