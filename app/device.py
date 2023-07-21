be2545430b2dd0e1599dbe14ae2d719204e2f68c
Félix Chambragne <felix.chambragne@gmail.com>
2023-07-21 13:57:01 +0200

    e

--- app/device.py
+++ app/device.py
@@ -43,8 +43,12 @@ class Device():
             if self.channels.get("white") and color == colors_constants["WHITE"]:
                 channels = [self.channels.get("red"), self.channels.get("green"), self.channels.get("blue"), self.channels.get("white")]
                 self.set_data(self.address, channels, [0, 0, 0, 255])
+            elif self.channels.get("white"):
+                channels = [self.channels.get("red"), self.channels.get("green"), self.channels.get("blue"), self.channels.get("white")]
+                self.set_data(self.address, channels, list(color).append(0))
             else:
                 self.set_data(self.address, channels, color)
+
             self.current_color = color
 
     def fade_color(self, channels, target_color, fade_duration):
